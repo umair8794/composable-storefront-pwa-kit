319f152b691d7da8ff0c18ac88f6bb4bf36fc8d4
Umair Farooq <umair.farooq@osf-commerce.com>
2024-01-12 18:55:00 +0500

    Added OCAPI call to content search to show contents in list for 'about' query

--- app/pages/content-search/index.jsx
+++ app/pages/content-search/index.jsx
@@ -1,6 +1,47 @@
 import React from 'react'
-const ContentSearch = () => {
-    return <div>Content Search</div>
+import fetch from 'cross-fetch'
+import {List, ListItem, Link} from '@chakra-ui/react'
+
+const ContentSearch = (props) => {
+    if (!props) {
+        return <div>Loading...</div>
+    }
+
+    const hits = props.hits
+    return (
+        <div>
+            {hits.length ? (
+                <List>
+                    {hits.map(({id, name}) => (
+                        <Link key={id} to={`/content/${id}`}>
+                            <ListItem>{name}</ListItem>
+                        </Link>
+                    ))}
+                </List>
+            ) : (
+                <div>No Content Items Found!</div>
+            )}
+        </div>
+    )
+}
+
+ContentSearch.getProps = async () => {
+    let contentResult
+    let siteId = 'RefArch'
+    let clientId = '66e30f6b-deeb-43b2-b99a-5192fe16d6d9'
+    let ocapiVersion = 'v23_2'
+    //Make a call to the URL substituting Key Values from table
+    const res = await fetch(
+        `http://localhost:3000/mobify/proxy/ocapi/s/${siteId}/dw/shop/${ocapiVersion}/content_search?q=about&client_id=${clientId}`
+    )
+    if (res.ok) {
+        contentResult = await res.json()
+    }
+    if (process.env.NODE_ENV !== 'production') {
+        console.log(contentResult)
+    }
+
+    return {hits: contentResult ? contentResult.hits : []}
 }
 
 ContentSearch.getTemplateName = () => 'content-search'
