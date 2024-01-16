d5b6c9637a89af3b70f0456152cf52f9f221477b
Umair Farooq <umair.farooq@osf-commerce.com>
2024-01-16 17:32:13 +0500

    Error handling with msg instead of exception throwing

--- app/pages/content-details/index.jsx
+++ app/pages/content-details/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import fetch from 'cross-fetch'
 import {useQuery} from '@tanstack/react-query'
 import {useParams} from 'react-router-dom'
-import {HTTPError} from 'pwa-kit-react-sdk/ssr/universal/errors'
 
 const ContentDetails = () => {
     const params = useParams()
@@ -11,19 +10,15 @@ const ContentDetails = () => {
     let clientId = '66e30f6b-deeb-43b2-b99a-5192fe16d6d9'
     let ocapiVersion = 'v23_2'
 
-    const {data, error, isLoading} = useQuery({
-        queryKey: [params.id],
-        queryFn: () => {
-            return fetch(
-                `http://localhost:3000/mobify/proxy/ocapi/s/${siteId}/dw/shop/${ocapiVersion}/content/${params.id}?client_id=${clientId}`
-            )
-                .then((res) => res.json())
-                .then((json) => {
-                    console.log(json)
-                    return json
-                })
-        },
-        cacheTime: 0
+    const {data, error, isLoading} = useQuery([params.id], () => {
+        return fetch(
+            `http://localhost:3000/mobify/proxy/ocapi/s/${siteId}/dw/shop/${ocapiVersion}/content/${params.id}?client_id=${clientId}`
+        )
+            .then((res) => res.json())
+            .then((json) => {
+                console.log(json)
+                return json
+            })
     })
 
     if (isLoading) {
@@ -31,7 +26,7 @@ const ContentDetails = () => {
     } else if (error) {
         return <div>Error query hit: {error}</div>
     } else if (data.fault) {
-        throw new HTTPError(404, data.fault.message)
+        return <div>{data.fault.message}</div>
     } else {
         return <div dangerouslySetInnerHTML={{__html: data.c_body}} />
     }
